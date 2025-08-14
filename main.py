f6893100c0b4b2dedcc407c8039cb98285d2363c
tricksterquill <amystiras@gmail.com>
2025-08-14 10:53:42 -0400

    implemented game_clock to manage fps

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    game_clock = pygame.time.Clock()
+    dt = 0
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
@@ -13,6 +17,10 @@ def main():
                 return
         screen.fill("black", rect=None, special_flags=0)
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
+        print(f"FPS: {game_clock.get_fps():.2f}")
+
+
 
 if __name__ == "__main__":
     main()
