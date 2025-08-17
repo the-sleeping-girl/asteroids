48112d7f936ed185a81760c80fa5186b1e0a6a4e
tricksterquill <amystiras@gmail.com>
2025-08-17 14:10:08 -0400

    Drew triandle and made go round.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -8,6 +9,7 @@ def main():
     
     game_clock = pygame.time.Clock()
     dt = 0
+    player_user = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -16,9 +18,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black", rect=None, special_flags=0)
+        player_user.update(dt)
+        player_user.draw(screen)
         pygame.display.flip()
         dt = game_clock.tick(60) / 1000
-        print(f"FPS: {game_clock.get_fps():.2f}")
+        # print(f"FPS: {game_clock.get_fps():.2f}")
+
 
 
 
